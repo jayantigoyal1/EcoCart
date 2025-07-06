dcc7ab8493267304a6123d01b0f28bd294ecec1b
jayantigoyal1 <jayantigoyal1911@gmail.com>
2025-07-06 16:19:58 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path'; // ✅ needed for alias
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()], // ✅ no need for tailwind plugin
+  plugins: [react()],
+  base: '/EcoCart/',
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'), // ✅ enables @/ for imports
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
