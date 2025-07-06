5a004c15dc6e7df68d0185d15a59fd99e96a2850
Jayanti Goyal <jayantigoyal1911@gmail.com>
2025-07-06 17:29:51 +0530

    fix: updated input component

--- src/components/ui/input.jsx
+++ src/components/ui/input.jsx
@@ -1,21 +1,8 @@
-<<<<<<< HEAD
-// components/ui/input.jsx
-export function Input({ className = "", ...props }) {
-  return (
-    <input
-      className={`w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-eco-green border ${className}`}
-      {...props}
-    />
-  );
+export function Input({ className = "", ...props}) {
+    return (
+        <input
+                className={`w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-eco-greeb border ${className}`}
+                {...props}
+        />
+    )
 }
-=======
-// components/ui/input.jsx
-export function Input({ className = "", ...props }) {
-  return (
-    <input
-      className={`w-full px-3 py-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-eco-green border ${className}`}
-      {...props}
-    />
-  );
-}
->>>>>>> dcc7ab8493267304a6123d01b0f28bd294ecec1b
