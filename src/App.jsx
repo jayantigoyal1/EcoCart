8b7a1c74dc8da4c41c5606006c3414510ddd7481
Jayanti Goyal <jayantigoyal1911@gmail.com>
2025-07-06 17:41:52 +0530

    updated router

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="flex justify-center bg-gray-100 min-h-screen">
       <div className="w-full max-w-sm">
-        <Router>
+        <Router basename="/EcoCart">
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
